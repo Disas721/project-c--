68b5046c58fb074187a183a93dd559f0ed8332b7
Groshev Vsevolod <groshev.vv@phystech.edu>
2022-05-20 00:02:08 +0300

    Попробовал

---
+++ queue.hpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+
+#pragma once
+
+class QueueElement {
+  public:
+    int _value;
+    int _key;
+
+    QueueElement(int value, int key);
+};
+
+class Queue {
+  private:
+    std::vector< QueueElement > _queue;
+    
+    void siftUp(int index);
+
+    void siftDown(int index);
+  
+  public:
+    void insert(int value, int key);
+
+    int extractMax();
+
+    void show();
+
+};
